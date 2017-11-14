cff35b9b45c89e60b05129a1b658c34fe3026972
Sean Scully <sean.erik.scully@gmail.com>
2017-11-14 16:22:22 +0100

    NRP-2276 Remove unused service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TravelSearchUI from './TravelSearchUI';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<TravelSearchUI />, document.getElementById('root'));
-registerServiceWorker();
