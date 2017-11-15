d3ea36bdbef67908587bad3cd02462faf8bf2e50
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-11-15 17:31:22 +0100

    Add script for uploading app to gcp

---
+++ upload_gcp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE_URI=$1
+
+if [ -z "$BASE_URI" ]; then
+  echo "USAGE: $0 gs://otpreport-test.entur.org/otp-travelsearch-qa"
+  exit 1
+fi
+
+echo "Uploading build directory content to $BASE_URI"
+gsutil -m cp -r build/* $BASE_URI
+
+# AVoid doing this for reports directory,
+
+gsutil -m acl ch -u AllUsers:R $BASE_URI/*
+gsutil -m acl ch -r -u AllUsers:R $BASE_URI/static/
