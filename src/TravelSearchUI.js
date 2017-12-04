005565fc3c3ed4bc84b3bf27b78631621732e7ec
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-12-04 16:20:33 +0100

    NRP-2388 Remove unused import

--- src/TravelSearchUI.js
+++ src/TravelSearchUI.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import ReportList from './ReportList';
 import { IntlProvider } from 'react-intl';
-import nb from 'react-intl/locale-data/nb';
-
-
 
 class TravelSearchUI extends Component {
   render() {
