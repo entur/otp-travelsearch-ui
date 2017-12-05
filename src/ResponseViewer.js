dfe8affaae7056d34dfcdeee570863cd7307a988
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-12-05 11:41:10 +0100

    NRP-2388 Stop propgation of onClick on main modal box!

--- src/ResponseViewer.js
+++ src/ResponseViewer.js
@@ -21,7 +21,7 @@ class ResponseViewer extends React.Component {
     if(this.state.opened) {
 
       return (<div className="modal-background" onClick={() => this.close()}>
-        <div className="modal-front">
+        <div className="modal-front" onClick={(e) => e.stopPropagation()}>
           {this.props.failedSearch.response}
         </div>
       </div>)
