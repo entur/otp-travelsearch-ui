2ae89d20118d4de7a23157b6a1da58e4a2aaba9e
Tom Erik Støwer <testower@gmail.com>
2021-06-14 19:52:57 +0200

    Disable parallell processing for gsutil due to macos issue

--- upload_gcp.sh
+++ upload_gcp.sh
@@ -8,10 +8,10 @@ if [ -z "$REPORT_BASE_URI" ]; then
 fi
 
 echo "Uploading build directory content to $REPORT_BASE_URI"
-gsutil -h "Cache-Control:private" -m cp -r build/* $REPORT_BASE_URI
+gsutil -o "GSUtil:parallel_process_count=1" -h "Cache-Control:private" -m cp -r build/* $REPORT_BASE_URI
 
-gsutil -m acl ch -u AllUsers:R $REPORT_BASE_URI/*
+gsutil -o "GSUtil:parallel_process_count=1" -m acl ch -u AllUsers:R $REPORT_BASE_URI/*
 
 # Avoid doing this for the reports directory,
-gsutil -m acl ch -r -u AllUsers:R $REPORT_BASE_URI/js/*
-gsutil -m acl ch -r -u AllUsers:R $REPORT_BASE_URI/css/*
+gsutil -o "GSUtil:parallel_process_count=1" -m acl ch -r -u AllUsers:R $REPORT_BASE_URI/js/*
+gsutil -o "GSUtil:parallel_process_count=1" -m acl ch -r -u AllUsers:R $REPORT_BASE_URI/css/*
